179d7169822cfef6501844a89434614d7f44aab1
brianvasqueza <alexandervasquez0082@gmail.com>
2024-11-23 16:59:35 -0600

    meta

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -6,9 +6,8 @@ import { getCollectionsWithProducts } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
-  description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+  title: "Mary Moda Exclusiva",
+  description: "Redefine your style with our exclusive elegant apparel.",
 }
 
 export default async function Home({
